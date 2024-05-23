b690667eb7235c0b53c523bcc9dc4458d722f790
alemran22 <alemrangazi@gmail.com>
2024-05-23 16:05:15 +0600

    slider page added for testing

--- script.js
+++ script.js
@@ -9,3 +9,47 @@ burgerMenu.addEventListener("click", function (){
 mobileNavShadow.addEventListener("click", function () {
     mobileNavSec.style.removeProperty("display")
 })
+
+//slick slider
+$(document).ready(function(){
+  $('.flashSale_container').slick({
+      // setting-name: setting-value
+      arrows: true,
+      prevArrow: ".backarrow",
+      nextArrow: ".nextarrow",
+      // dots: true,
+//   infinite: true,
+//   speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      // fade: true,
+       
+      
+  });
+     $(".categorySlideContainer").slick({
+       // setting-name: setting-value
+       arrows: true,
+       prevArrow: ".categoryBackarrow",
+       nextArrow: ".categoryNextarrow",
+       // dots: true,
+       //   infinite: true,
+       //   speed: 300,
+       slidesToShow: 4,
+       slidesToScroll: 1,
+       // fade: true,
+     });
+    
+    
+     $(".productSlideContainer").slick({
+       // setting-name: setting-value
+       arrows: true,
+       prevArrow: ".productsBackArrow",
+       nextArrow: ".productsNextArrow",
+       // dots: true,
+       //   infinite: true,
+       //   speed: 300,
+       slidesToShow: 4,
+       slidesToScroll: 1,
+       // fade: true,
+     });
+    })
